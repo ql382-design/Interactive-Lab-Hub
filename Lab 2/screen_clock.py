b1f7b05961379a75e41624bdb1ccbaadeecb8bb5
ql382-design <ql382@cornell.edu>
2025-09-13 15:46:52 -0400

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -61,11 +61,15 @@ backlight.switch_to_output()
 backlight.value = True
 
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
-
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-
-    # Display image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    current_time = time.strftime("%H:%M:%S")
+    draw.text((10, height // 2), current_time, font=font, fill=(255, 255, 255))
     disp.image(image, rotation)
     time.sleep(1)
+
+
+
+
+
+
+
